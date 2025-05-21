3950e6a08ad3a807f1b4e199012e3ffacc682e8f
lavanya9755 <lavanyachawla24@gmail.com>
2025-05-21 14:53:32 +0530

    in memory roles user+admin

--- src/main/java/com/demo/oauth/Controller.java
+++ src/main/java/com/demo/oauth/Controller.java
@@ -1,6 +1,7 @@
 package com.demo.oauth;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,6 +13,18 @@ public class Controller {
     public String sayHellp() {
         return "hello";
     }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String userEndpoint() {
+        return "hello user!";
+    }
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')") //security before method
+    public String adminEndpoint() {
+        return "hello admin!";
+    }
+    //note: there s one more annotation,PostAuthorize() were security will be doen after method result 
     
     
 }
--- src/main/java/com/demo/oauth/SecurityConfig.java
+++ src/main/java/com/demo/oauth/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -17,14 +21,41 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable()); //csrf disble
-        http.authorizeHttpRequests(authorizeHttpRequests -> 
-        authorizeHttpRequests.anyRequest().authenticated()
+        http.authorizeHttpRequests((authorizeHttpRequests) -> 
+                                    authorizeHttpRequests.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated()
         );
-        http.oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:3000/dashboard",true)); //git integeration
+        // http.oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:3000/dashboard",true)); //git integeration
         // http.formLogin(form -> form.defaultSuccessUrl("/hello",true)); //basic shit it was
-
+        // http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())); //allowign headers frames
         return http.build();
 
     }
+
+    //note: inmemoryuserdetailmanager extends userdetail manager and userdetailpassword
+    //userdetailmanager extends userdetailService interface
+    //therfore inmemeoryuserdetailmanager is implementation os userdetailService
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                                .password("{noop}password1") //noop -prefix,used to save as plain text
+                                .roles("USER")
+                                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                                .password("{noop}adminPass") 
+                                .roles("ADMIN")
+                                .build(); 
+                                
+        //create user in Database
+        // JdbcUserDetailsManager userdet
+
+        return new InMemoryUserDetailsManager(user1,admin);
+    }
+
+     
+    
+
     
 }
